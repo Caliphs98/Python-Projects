7de1547b879cece13e30eab10cf0d780d1cc1396
Caliphs98 <147924046+Caliphs98@users.noreply.github.com>
2024-03-19 16:45:57 +0200

    new

---
+++ Stock prices collector/main.py
@@ -0,0 +1,14 @@
+from stock import Stock
+
+symbols = ['MSFT', 'GOOGL', 'AAPL', 'META']
+threads = []
+
+for symbol in symbols:
+    t = Stock(symbol)
+    threads.append(t)
+    t.start()
+    
+for t in threads:
+    t.join()
+    print(f' {t}')
+    
